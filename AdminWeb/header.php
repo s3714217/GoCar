f3323ca31f47d4eff7b9f9374e37b17a635a9b6e
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-12 16:22:22 +1100

    Update header.php

--- AdminWeb/header.php
+++ AdminWeb/header.php
@@ -1,22 +1,44 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary sticky-top">
-    <a class="navbar-brand ml-lg-4" href="">
-        <img src="" class="d-inline-block align-top w-100" alt="GoCar">
-    </a>
-    
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigationBar" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-
-    <div class="collapse navbar-collapse ml-lg-4" id="navigationBar">
-        <ul class="navbar-nav">
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="">About Us</a>
+                <a class="nav-link" href="index.php">
+                    <img src="style/dashboard-24px 1.png" alt="dashboardIcon">
+                    Dashboard
+                </a>
             </li>
-
             <li class="nav-item">
-                <a class="nav-link" href="">Book Cars</a>
+                <a class="nav-link" href="#">
+                    <img src="style/drive_eta-24px 1.png" alt="carIcon">
+                    Car
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="style/local_parking-24px 1.png" alt="parkingIcon">
+                    Parking
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="style/people-24px 1.png" alt="userIcon">
+                    User Central
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="style/perm_identity-24px 1.png" alt="adminIcon">
+                    Admin
+                </a>
             </li>
         </ul>
     </div>
+    
+    <!--<a class="navbar-brand ml-lg-4" href="">
+        <img src="" class="d-inline-block align-top w-100" alt="GoCar">
+    </a>-->
+
+    <!--<div class="collapse navbar-collapse ml-lg-4" id="navigationBar">
+    </div>-->
 
 </nav>
