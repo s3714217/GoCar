2d50d66583f140a09d321736a548a01e48c750c8
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-13 18:51:56 +1100

    Add files via upload

---
+++ AdminWeb/index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if(isset($_SESSION['portal_logged_in']) && $_SESSION['portal_logged_in'] == TRUE)
+{
+    switch (parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':
+            require 'register.php';
+            break;
+        case '/register':
+            require 'register.php';
+            break;
+        case '/main':
+            require 'main.php';
+            break;
+        case '/logout':
+            require 'logout.php';
+            break;
+        default:
+            http_response_code(404);
+            exit('Not Found');
+    }
+} else {
+    switch (parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':
+            require 'login.php';
+            break;
+        default:
+            http_response_code(404);
+            exit('Not Found');
+}
+}
+?>
