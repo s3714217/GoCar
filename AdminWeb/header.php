df68d6ae5cf922503ae0b60e0d4f5fd02825971f
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-13 18:53:29 +1100

    Update header.php

--- AdminWeb/header.php
+++ AdminWeb/header.php
@@ -1,34 +1,45 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-    <div class="position-sticky pt-3">
+<nav id="sidebarMenu" class="d-md-block bg-light sidebar collapse h-100">
+    <div class="position-sticky pt-5">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="index.php">
-                    <img src="style/dashboard-24px 1.png" alt="dashboardIcon">
+                <a class="nav-link" href="main">
+                    <img src="style/logo.svg" alt="gocarIcon">
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="main">
+                    <img src="style/dashboard-24px 1.svg" alt="dashboardIcon">
                     Dashboard
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="style/drive_eta-24px 1.png" alt="carIcon">
+                    <img src="style/drive_eta-24px 1.svg" alt="carIcon">
                     Car
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="style/local_parking-24px 1.png" alt="parkingIcon">
+                    <img src="style/local_parking-24px 1.svg" alt="parkingIcon">
                     Parking
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="style/people-24px 1.png" alt="userIcon">
+                    <img src="style/people-24px 1.svg" alt="userIcon">
                     User Central
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="style/perm_identity-24px 1.png" alt="adminIcon">
+                    <img src="style/perm_identity-24px 1.svg" alt="adminIcon">
                     Admin
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/logout">
+                    <img src="style/logout-24px 1.svg" alt="logoutIcon">
+                    Logout
                 </a>
             </li>
         </ul>
