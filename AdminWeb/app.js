b165c660e713353c7e07aaecb92875c5c5a6adbb
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-14 12:06:43 +1100

    Add files via upload

---
+++ AdminWeb/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyASKJm9Imw32FCaCdQotxlHqVvFNumFsbI",
+    authDomain: "gocarrmit.firebaseapp.com",
+    projectId: "gocarrmit",
+    storageBucket: "gocarrmit.appspot.com",
+    messagingSenderId: "213045637799",
+    appId: "1:213045637799:web:8ab7ba26e7fe50feb46182",
+    measurementId: "G-F84CS7CZCL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
