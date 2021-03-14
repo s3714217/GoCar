8cca3b1c1051dd24e4a77ae5cfdd0f2a9de23f5b
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-14 17:18:41 +1100

    Update main.php

--- AdminWeb/main.php
+++ AdminWeb/main.php
@@ -29,20 +29,98 @@
 
 <body>
     <!--Main Dashboard-->
-    <div id="landing-page">
-        <div id="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <h2>Dashboard</h2>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3">
+                <?php include 'header.php'; ?>    
+            </div>
+
+            <div class="col-9">
+                <div class="container-fluid my-3 footer-align-bottom">
+                    <h1 class="text-left font-weight-bold ml-15">Dashboard</h1>
+                    <p class="ml-15"><?php echo date('jS F Y'); ?></p>
+
+                    <div class="row mt-5">
+                        <div class="col-3 border shadow bg-white rounded-lg p-3">
+                            <div class="d-flex">
+                                <h5 class="my-auto text-black-50 font-weight-bold">Total Users</h5>
+
+                                <span class="ml-auto">
+                                    <img src="style/total_users.svg" alt="Total Users Icon">
+                                </span>
+                            </div>
+
+                            <p class="h2 font-weight-bold">200</p>
+                        </div>
+
+                        <div class="col px-0"></div>
+
+                        <div class="col-3 border shadow bg-white rounded-lg p-3">
+                            <div class="d-flex">
+                                <h5 class="my-auto text-black-50 font-weight-bold">Total Cars Available</h5>
+
+                                <span class="ml-auto">
+                                    <img src="style/total_cars_available.svg" alt="Total Cars Icon">
+                                </span>
+                            </div>
+
+                            <p class="h2 font-weight-bold text-success">200</p>
+                        </div>
+
+                        <div class="col"></div>
+
+                        <div class="col-3 border shadow bg-white rounded-lg p-3">
+                            <div class="d-flex">
+                                <h5 class="my-auto text-black-50 font-weight-bold">Parking Available</h5>
+
+                                <span class="ml-auto">
+                                    <img src="style/parking_available.svg" alt="Parking Available Icon">
+                                </span>
+                            </div>
+
+                            <p class="h2 font-weight-bold">40</p>
+                        </div>
+                    </div>
+
+                    <!--Notifications row-->
+                    <div class="row mt-5">
+                        <div class="col border shadow bg-white rounded-lg p-3">
+                            <h5 class="my-auto text-black-50 font-weight-bold">Notifications</h5>
+
+                            <p class="h2 font-weight-bold">40</p>
+                        </div>
+                    </div>
+
+                    <!--Notifications row-->
+                    <div class="row mt-5">
+                        <div class="col-9 border shadow bg-white rounded-lg p-3">
+                            <div class="d-flex">
+                                <h5 class="my-auto text-black-50 font-weight-bold">Recent Bookings</h5>
+
+                                <span class="ml-auto">
+                                    <a href="#">
+                                        More
+                                    
+                                        <img src="style/arrow_right_alt-24px 1.svg" alt="Right Arrow Icon">
+                                    </a>
+                                </span>
+                            </div>
+
+                            <p class="h2 font-weight-bold">40</p>
+                        </div>
+
+                        <div class="col-1 px-0"></div>
+
+                        <div class="col-2 border shadow bg-white rounded-lg p-3">
+                            <h5 class="my-auto text-black-50 font-weight-bold">Recent Activity</h5>
+                        </div>
                     </div>
                 </div>
+
+                <?php include 'footer.php'; ?>
             </div>
         </div>
     </div>
-    <?php include 'header.php'; ?>
-
-    <?php include 'footer.php'; ?>
 </body>
 
 </html>
