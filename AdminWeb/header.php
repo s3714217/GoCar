495c748e7a5568cc11c55dc8a21f09fba7845d22
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-03-14 17:17:41 +1100

    Update header.php

--- AdminWeb/header.php
+++ AdminWeb/header.php
@@ -1,55 +1,45 @@
 <nav id="sidebarMenu" class="d-md-block bg-light sidebar collapse h-100">
-    <div class="position-sticky pt-5">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="main">
-                    <img src="style/logo.svg" alt="gocarIcon">
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="main">
-                    <img src="style/dashboard-24px 1.svg" alt="dashboardIcon">
-                    Dashboard
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <img src="style/drive_eta-24px 1.svg" alt="carIcon">
-                    Car
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <img src="style/local_parking-24px 1.svg" alt="parkingIcon">
-                    Parking
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <img src="style/people-24px 1.svg" alt="userIcon">
-                    User Central
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <img src="style/perm_identity-24px 1.svg" alt="adminIcon">
-                    Admin
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/logout">
-                    <img src="style/logout-24px 1.svg" alt="logoutIcon">
-                    Logout
-                </a>
-            </li>
-        </ul>
-    </div>
-    
-    <!--<a class="navbar-brand ml-lg-4" href="">
-        <img src="" class="d-inline-block align-top w-100" alt="GoCar">
-    </a>-->
-
-    <!--<div class="collapse navbar-collapse ml-lg-4" id="navigationBar">
-    </div>-->
-
+    <ul class="nav flex-column">
+        <li class="nav-item mt-5">
+            <a class="nav-link" href="main">
+                <img src="style/dashboard_logo.svg" alt="gocarIcon">
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="main">
+                <img src="style/dashboard-24px 1.svg" alt="dashboardIcon">
+                Dashboard
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="#">
+                <img src="style/drive_eta-24px 1.svg" alt="carIcon">
+                Car
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="#">
+                <img src="style/local_parking-24px 1.svg" alt="parkingIcon">
+                Parking
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="#">
+                <img src="style/people-24px 1.svg" alt="userIcon">
+                User Central
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="#">
+                <img src="style/perm_identity-24px 1.svg" alt="adminIcon">
+                Admin
+            </a>
+        </li>
+        <li class="nav-item my-3">
+            <a class="nav-link" href="/logout">
+                <img src="style/logout-24px 1.svg" alt="logoutIcon">
+                Logout
+            </a>
+        </li>
+    </ul>
 </nav>
