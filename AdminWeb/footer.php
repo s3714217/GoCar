fa8fa8b1be34aae7b98a2bf5bdbd34183be140cb
s3730387 <42113257+s3730387@users.noreply.github.com>
2021-04-11 12:28:36 +1000

    Delete AdminWeb directory

--- AdminWeb/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer container-fluid py-2 bg-secondary text-logo mt-5 d-flex">
-    <h6 class="text-center m-auto">Copyright &copy; <?php echo date('Y'); ?> &#64; gocar.com</h6>
-</div>
--- AdminWeb/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require __DIR__ . '/vendor/autoload.php';
-    session_start();
-
-    session_unset();
-
-    header('location:/');
-    exit;
-?>
